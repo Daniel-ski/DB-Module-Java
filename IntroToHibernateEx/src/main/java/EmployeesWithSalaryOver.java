2b89f3d9f9106726f39c9ff212ce3b3b0fa0b34f
Daniel Ivanov <danielskiapple@gmail.com>
2024-02-17 23:49:27 +0200

    Create EmployeesWithSalaryOver.java

---
+++ IntroToHibernateEx/src/main/java/EmployeesWithSalaryOver.java
@@ -0,0 +1,21 @@
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmployeesWithSalaryOver {
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.createQuery("SELECT e.firstName FROM Employee e WHERE e.salary > 50000", String.class)
+                .getResultList()
+                .forEach(System.out::println);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
