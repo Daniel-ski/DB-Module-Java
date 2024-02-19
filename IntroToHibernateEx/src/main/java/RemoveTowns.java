e9942d44551e2d235a146dce89df22a6be939fee
Daniel Ivanov <danielskiapple@gmail.com>
2024-02-19 23:13:07 +0200

    Create RemoveTowns.java

---
+++ IntroToHibernateEx/src/main/java/RemoveTowns.java
@@ -0,0 +1,46 @@
+import entities.Address;
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoveTowns {
+    public static void main(String[] args) {
+
+        EntityManager entityManager =
+                Persistence.createEntityManagerFactory("PU_Name").createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        final String townName = new Scanner(System.in).nextLine();
+
+        final Town townDelete = entityManager.createQuery("FROM Town "
+                        + "WHERE name = :town", Town.class)
+                .setParameter("town", townName)
+                .getSingleResult();
+
+        final List<Address> addressesToDelete = entityManager
+                .createQuery("FROM Address WHERE town.id= :id", Address.class)
+                .setParameter("id", townDelete.getId())
+                .getResultList();
+
+        addressesToDelete
+                .forEach(t -> t.getEmployees()
+                        .forEach(em -> em.setAddress(null)));
+
+        addressesToDelete.forEach(entityManager::remove);
+        entityManager.remove(townDelete);
+
+        final int countDeletedAddresses = addressesToDelete.size();
+
+        System.out.printf("%d address%s in %s deleted",
+                countDeletedAddresses,
+                countDeletedAddresses == 1 ? "" : "es",
+                townName);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
