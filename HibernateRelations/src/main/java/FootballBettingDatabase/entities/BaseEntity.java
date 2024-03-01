2508e52c52b057b38167e988ca69e91f8ccdce99
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-01 19:11:38 +0200

    Create BaseEntity.java

---
+++ HibernateRelations/src/main/java/FootballBettingDatabase/entities/BaseEntity.java
@@ -0,0 +1,11 @@
+package FootballBettingDatabase.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
