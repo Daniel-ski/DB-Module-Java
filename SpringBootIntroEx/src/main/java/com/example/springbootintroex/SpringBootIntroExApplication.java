d56ca6d835fad5dafeef142eb2adecb7cb9ac12f
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-07 12:52:43 +0200

    Create SpringBootIntroExApplication.java

---
+++ SpringBootIntroEx/src/main/java/com/example/springbootintroex/SpringBootIntroExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootintroex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroExApplication.class, args);
+    }
+
+}
