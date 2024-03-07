733d112f67262d1f172c3eb06a3676c7a2b86697
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-07 12:52:27 +0200

    Create CategoryServiceImpl.java

---
+++ SpringBootIntroEx/src/main/java/BookshopSystem/services/category/CategoryServiceImpl.java
@@ -0,0 +1,15 @@
+package BookshopSystem.services.category;
+
+import BookshopSystem.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
