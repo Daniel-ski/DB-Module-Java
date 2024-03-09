fe76fa26f43384a7e3c61396b29d1c66659bbac7
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-09 17:06:57 +0200

    Create SpringBootIntroExApplication.java

---
+++ SpringBootIntroEx/src/main/java/BookshopSystem/SpringBootIntroExApplication.java
@@ -0,0 +1,13 @@
+package BookshopSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroExApplication.class, args);
+    }
+
+}
