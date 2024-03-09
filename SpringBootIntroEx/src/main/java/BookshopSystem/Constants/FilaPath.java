ee48f541bc7f33e3c26f157f9e45144acd4838f4
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-09 17:06:33 +0200

    Create FilaPath.java

---
+++ SpringBootIntroEx/src/main/java/BookshopSystem/Constants/FilaPath.java
@@ -0,0 +1,9 @@
+package BookshopSystem.Constants;
+
+public enum FilaPath {
+    ;
+    public static final String RESOURCES_URL = "src/main/resources/dbContent/";
+    public static final String BOOK_FILE_NAME = "books.txt";
+    public static final String AUTHOR_FILE_NAME = "authors.txt";
+    public static final String CATEGORY_FILE_NAME = "categories.txt";
+}
