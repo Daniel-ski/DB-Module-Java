7a9db221dc4053592dfecca4fe0cf983a8c0554f
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-11 22:43:29 +0200

    Create UserRepository.java

---
+++ SpringBootIntro/src/main/java/com/example/springbootintro/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springbootintro.repositories;
+
+import com.example.springbootintro.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUserName(String userName);
+
+}
