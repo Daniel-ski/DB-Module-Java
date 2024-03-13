fba369ca1e8f0d5bbe2396992d454f0528496f8b
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-13 17:13:01 +0200

    Update Author.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/domain/entities/Author.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/domain/entities/Author.java
@@ -1,13 +1,11 @@
 package BookshopSystem.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
-@Entity
+@Entity(name = "authors")
 @Table
 public class Author extends BaseEntity {
     @Column(name = "first_name")
@@ -15,6 +13,38 @@ public class Author extends BaseEntity {
     @Column(name = "last_name",nullable = false)
     private String lastName;
 
-    @OneToMany
+    @OneToMany(targetEntity = Book.class,mappedBy = "author",fetch = FetchType.EAGER)
     private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
