d0aa13dc7f3f1bda325f01ee353bf4424e487bb2
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-13 17:12:35 +0200

    Update AuthorService.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/services/author/AuthorService.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/services/author/AuthorService.java
@@ -1,4 +1,15 @@
 package BookshopSystem.services.author;
 
+import BookshopSystem.domain.entities.Author;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AuthorService {
+    void seedAuthors(List<Author> authors);
+    boolean isDataSeeded();
+    Author getRandomAuthor();
+    List<Author> getDistinctByBooksReleaseDateBefore(LocalDate books_releaseDate);
+
+    void getAuthorsOrderByNumberOfBooks();
 }
