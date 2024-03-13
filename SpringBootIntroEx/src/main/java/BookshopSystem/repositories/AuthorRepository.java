8d8ae9a892cf992954c8b56875680a587f2b1c3c
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-13 17:13:36 +0200

    Update AuthorRepository.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/repositories/AuthorRepository.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/repositories/AuthorRepository.java
@@ -4,6 +4,12 @@ import BookshopSystem.domain.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+    List<Author> getDistinctByBooksReleaseDateBefore(LocalDate books_releaseDate);
+
 }
