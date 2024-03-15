34c80ab1a63070456305576ee0326fe53464c7de
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-15 23:34:13 +0200

    Update BookService.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/services/book/BookService.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/services/book/BookService.java
@@ -1,4 +1,20 @@
 package BookshopSystem.services.book;
 
+import BookshopSystem.domain.entities.Book;
+import BookshopSystem.domain.enums.AgeRestriction;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface BookService {
+    void seedBooks(List<Book> books);
+
+    boolean isDataSeeded();
+
+    ArrayList<Book> getBooksByReleaseDateAfter(LocalDate releaseDate);
+
+    List<Book> getBooksByAuthorNameOrderByReleaseDateDesc(String firstName, String lastName);
+
+    List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction);
 }
