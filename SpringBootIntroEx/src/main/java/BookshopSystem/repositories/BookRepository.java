f3141a0f3db8e4a07843d0af164427ba98223234
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-15 23:34:30 +0200

    Update BookRepository.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/repositories/BookRepository.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/repositories/BookRepository.java
@@ -1,9 +1,20 @@
 package BookshopSystem.repositories;
 
 import BookshopSystem.domain.entities.Book;
+import BookshopSystem.domain.enums.AgeRestriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
+    ArrayList<Book> getBooksByReleaseDateAfter(LocalDate releaseDate);
+
+    ArrayList<Book> getBooksByAuthor_FirstNameAndAuthor_LastNameOrderByReleaseDateDesc(String firstName, String lastName);
+
+    Optional< List<Book>> findAllByAgeRestriction(AgeRestriction ageRestriction);
 }
