4a5816720ceeb1bb3a35eb127b1e9ea8be575c28
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-15 23:34:22 +0200

    Update BookServiceImpl.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/services/book/BookServiceImpl.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/services/book/BookServiceImpl.java
@@ -1,8 +1,17 @@
 package BookshopSystem.services.book;
 
+import BookshopSystem.domain.entities.Book;
+import BookshopSystem.domain.enums.AgeRestriction;
 import BookshopSystem.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class BookServiceImpl implements BookService{
@@ -11,5 +20,33 @@ public class BookServiceImpl implements BookService{
     @Autowired
     public BookServiceImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void seedBooks(List<Book> books) {
+        this.bookRepository.saveAllAndFlush(books);
+    }
+
+    @Override
+    public boolean isDataSeeded() {
+        return this.bookRepository.count() > 0;
+    }
+
+    @Override
+    public ArrayList<Book> getBooksByReleaseDateAfter(LocalDate releaseDate) {
+       return this.bookRepository.getBooksByReleaseDateAfter(releaseDate);
+
+    }
+
+    @Override
+    public List<Book> getBooksByAuthorNameOrderByReleaseDateDesc(String firstName, String lastName) {
+        return this.bookRepository.getBooksByAuthor_FirstNameAndAuthor_LastNameOrderByReleaseDateDesc(firstName, lastName)
+                .stream()
+                .sorted(Comparator.comparing(Book::getTitle)).toList();
+    }
+
+    @Override
+    public List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction) {
+        return this.bookRepository.findAllByAgeRestriction(ageRestriction).orElseThrow(NoSuchElementException::new);
     }
 }
