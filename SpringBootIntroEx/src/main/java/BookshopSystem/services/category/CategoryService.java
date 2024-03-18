92f09894c26f2ed70457e44a24baa84f3c99606c
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-18 23:46:58 +0200

    Update CategoryService.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/services/category/CategoryService.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/services/category/CategoryService.java
@@ -1,4 +1,14 @@
 package BookshopSystem.services.category;
 
+import BookshopSystem.domain.entities.Category;
+
+import java.util.List;
+import java.util.Set;
+
 public interface CategoryService {
+    void seedCategories(List<Category> categories);
+    boolean isDataSeeded();
+    Category getRandomCategory();
+
+    Set<Category> getRandomCategories();
 }
