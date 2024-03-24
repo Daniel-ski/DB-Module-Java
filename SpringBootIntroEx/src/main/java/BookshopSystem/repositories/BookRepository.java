0ed95958cd724e03480387a65d5a9923fa793810
Daniel Ivanov <danielskiapple@gmail.com>
2024-03-24 22:05:32 +0200

    Update BookRepository.java

--- SpringBootIntroEx/src/main/java/BookshopSystem/repositories/BookRepository.java
+++ SpringBootIntroEx/src/main/java/BookshopSystem/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package BookshopSystem.repositories;
 
+import BookshopSystem.domain.dto.BookDto;
 import BookshopSystem.domain.entities.Book;
 import BookshopSystem.domain.enums.AgeRestriction;
 import BookshopSystem.domain.enums.EditionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.xml.crypto.Data;
@@ -27,4 +29,11 @@ public interface BookRepository extends JpaRepository<Book,Long> {
     Optional<List<Book>> findAllByReleaseDateNot(LocalDate year);
 
     Optional<List<Book>> findAllByReleaseDateBefore(LocalDate date);
+
+    @Query("SELECT count(b) FROM Book AS b WHERE length(b.title) > :length ")
+    Optional<Integer> findBookCountByTitleIsGreaterThan(Integer length);
+
+    @Query("SELECT new BookshopSystem.domain.dto.BookDto(b.title,b.editionType,b.ageRestriction,b.price) " +
+            "FROM Book AS b WHERE b.title = :title")
+    Optional<List<BookDto>> findBooksByTitle(String title);
 }
