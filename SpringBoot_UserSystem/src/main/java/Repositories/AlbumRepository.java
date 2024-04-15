613137b2095962c5cf00a7b66c3e47a67d2d6abf
Daniel Ivanov <danielskiapple@gmail.com>
2024-04-15 23:49:35 +0300

    Create AlbumRepository.java

---
+++ SpringBoot_UserSystem/src/main/java/Repositories/AlbumRepository.java
@@ -0,0 +1,11 @@
+package Repositories;
+
+import domain.entities.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album,Long> {
+
+}
