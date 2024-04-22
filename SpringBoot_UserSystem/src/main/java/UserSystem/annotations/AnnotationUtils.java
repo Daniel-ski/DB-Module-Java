47f928f1e82c30cadb48a8cd11375963dd947521
Daniel Ivanov <danielskiapple@gmail.com>
2024-04-22 16:08:27 +0300

    Create AnnotationUtils.java

---
+++ SpringBoot_UserSystem/src/main/java/UserSystem/annotations/AnnotationUtils.java
@@ -0,0 +1,13 @@
+package UserSystem.annotations;
+
+import jakarta.validation.ConstraintValidatorContext;
+
+public enum AnnotationUtils {
+    ;
+
+    public static void setErrorMessage(final ConstraintValidatorContext context,final String errorMessage){
+
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
+    }
+}
