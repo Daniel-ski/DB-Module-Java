0296daeaeb803c1448c6721b6eca89ad90badff0
Daniel Ivanov <danielskiapple@gmail.com>
2024-04-28 21:51:47 +0300

    Create Password.java

---
+++ SpringBoot_UserSystem/src/main/java/UserSystem/annotations/password/Password.java
@@ -0,0 +1,31 @@
+package UserSystem.annotations.password;
+
+import UserSystem.constants.Constants;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+
+    String message() default Constants.INVALID_PASSWORD_FORMAT;
+    int minLength() default 6;
+    int maxLength() default 30;
+    boolean containsDigit() default false;
+    boolean containsLowerCase() default false;
+    boolean containsUpperCase() default false;
+    boolean containsSpecialSymbols() default false;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
